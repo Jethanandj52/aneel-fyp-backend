13a8487fb183921ad44aa2013c74574efbdaa46c
Jethanand <134337163+Jethanandj52@users.noreply.github.com>
2025-11-08 02:04:52 +0500

    first commit

--- src/app.js
+++ src/app.js
@@ -59,7 +59,8 @@ app.get("/", (req, res) => {
 });
 
 // ✅ Start Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server is running on port ${PORT}`);
+// });
+module.exports = app;
